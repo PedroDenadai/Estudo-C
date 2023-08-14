ddbd9b1ea9467d9de475c307601cfafafef157f3
PedroDenadai <93556114+PedroDenadai@users.noreply.github.com>
2023-08-14 08:09:14 -0300

    Update senha.c

--- Faculdade/senha.c
+++ Faculdade/senha.c
@@ -5,7 +5,7 @@ int main(void) {
     printf("Insira uma senha (Somente numeros): ");
     scanf("%d", &senha);
 
-    if (senha > 999) {
+    if (senha > 999 && senha <= 9999) {
         if (senha % 2 != 0 && senha % 17 != 0) {   
             printf("\nSenha forte\n");
         } else {
